cdc0af24ed7adfe97577f1d9a6b370e39f77420b
shayantan <shayantankabiraj@gmail.com>
2023-09-15 19:10:12 +0530

    Blogs Functional and Responsive. Colours and Fonts needs to be changed

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -5,6 +5,8 @@ import Navbar from './Navbar'
 import Footer from './Footer/Footer'
 import PageNotFound from './PageNotFound'
 
+import './Blog.css'
+
 function Blog() {
 
     const [ searchParams, setSearchParams ] = useSearchParams()
@@ -24,6 +26,18 @@ function Blog() {
         }
     }
 
+    var float = ['left', 'right']
+    var padding = [{'padding-left': '20px'}, {'padding-right': '20px'}]
+    var pointer = 1
+    function getFloat(){
+        if(pointer === 1){
+            return {float: float[pointer--], paddingLeft: '20px'}
+        }
+        else{
+            return {float: float[pointer++], paddingRight: '20px'}
+        }
+    }
+
     useEffect( () => {
 
         getAndSetBlog(searchParams.get('_id'))
@@ -33,16 +47,64 @@ function Blog() {
         return (
             <>
                 <Navbar />
-                <h1>{blog.thumbnailText}</h1>
-                <img src={`/images/${blog.thumbnailImg}`} style={{height: 300, width: 300}} />
                 
-                <div>
+                <div className='blog-content'>
+                    <h1>{blog.thumbnailText}</h1>
+                    {/* <img src={`/images/${blog.thumbnailImg}`} style={{height: 300, width: 300}} /> */}
                     {blog.content?.map((each, i) => (
-                        <>
-                            {each.header && (<h1 key={i}>{each.header}</h1>)}
+                        <div className='wrapper'>
+                            {(each.header && each.image && each.paragraph) && (
+                                <>
+                                    <img 
+                                        key={i}
+                                        className='display-image' 
+                                        style={getFloat()}
+                                        src={`/images/${each.image}`}
+                                    />
+                                    <div className='text-box'>
+                                        <h3 key={i}>{each.header}</h3>
+                                        <p key={i}>{each.paragraph}</p>
+                                    </div>
+                                </>
+                            ) ||
+                            (each.header && each.image) && (
+                                <>
+                                    <img
+                                        key={i}
+                                        src={`/images/${each.image}`}
+                                    />
+                                    <h3 key={i}>{each.header}</h3>
+                                </>
+                            ) ||
+                            (each.image && each.paragraph) && (
+                                <>
+                                    <img 
+                                        key={i}
+                                        className='display-image' 
+                                        style={getFloat()}
+                                        src={`/images/${each.image}`}
+                                    />
+                                    <div className='text-box'>
+                                        <p key={i}>{each.paragraph}</p>
+                                    </div>
+                                </>
+                            ) ||
+                            (each.header || each.paragraph) && (
+                                <>
+                                    {each.header && (<h3 key={i}>{each.header}</h3>)}
+                                    {each.paragraph && (<p key={i}>{each.paragraph}</p>)}
+                                </>
+                            ) || 
+                            each.image && (<img 
+                                    key={i} 
+                                    src={`/images/${each.image}`}
+                                />)
+                            }
+
+                            {/* {each.header && (<h1 key={i}>{each.header}</h1>)}
                             {each.image && (<img key={i} src={`/images/${each.image}`} />)}
-                            {each.paragraph && (<p key={i}>{each.paragraph}</p>)}
-                        </>
+                            {each.paragraph && (<p key={i}>{each.paragraph}</p>)} */}
+                        </div>
                     ))}
                 </div>
                 <Footer />
